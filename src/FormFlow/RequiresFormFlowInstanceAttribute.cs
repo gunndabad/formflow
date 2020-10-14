fdf89ac07dd5d952fb860e88305b01cd77db8b47
James Gunn <james@gunn.io>
2020-10-14 15:08:26 +0100

    Add RequiresFormFlowInstanceAttribute for annotating actions that require an instance

---
+++ src/FormFlow/RequiresFormFlowInstanceAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FormFlow.Filters;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace FormFlow
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+    public sealed class RequiresFormFlowInstanceAttribute : Attribute, IActionModelConvention, IControllerModelConvention
+    {
+        void IActionModelConvention.Apply(ActionModel action)
+        {
+            AddMetadataToAction(action);
+        }
+
+        void IControllerModelConvention.Apply(ControllerModel controller)
+        {
+            foreach (var action in controller.Actions)
+            {
+                AddMetadataToAction(action);
+            }
+        }
+
+        private void AddMetadataToAction(ActionModel action)
+        {
+            action.Properties.Add(typeof(RequiresInstanceMarker), RequiresInstanceMarker.Instance);
+        }
+    }
+}
