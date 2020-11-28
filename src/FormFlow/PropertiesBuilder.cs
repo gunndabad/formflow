af53219a6cc49ed2f04e7dfd190f558f9a9a8ec3
James Gunn <james@gunn.io>
2020-11-28 11:05:55 +0000

    Add builder type for creating instance properties dictionaries

---
+++ src/FormFlow/PropertiesBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace FormFlow
+{
+    public class PropertiesBuilder
+    {
+        private readonly Dictionary<object, object> _values;
+
+        public PropertiesBuilder()
+        {
+            _values = new Dictionary<object, object>();
+        }
+
+        public PropertiesBuilder Add(object key, object value)
+        {
+            _values.Add(key, value);
+            return this;
+        }
+
+        public IReadOnlyDictionary<object, object> Build() =>
+            new ReadOnlyDictionary<object, object>(_values);
+
+        public static IReadOnlyDictionary<object, object> CreateEmpty() =>
+            new PropertiesBuilder().Build();
+    }
+}
