15c9a3dad6107c5d872f39590b1ff3898efccefc
James Gunn <james@gunn.io>
2020-12-09 11:24:44 +0000

    Add KeysBuilder - a convenience type for creating keys dictionaries

---
+++ src/FormFlow/KeysBuilder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Microsoft.Extensions.Primitives;
+
+namespace FormFlow
+{
+    public class KeysBuilder
+    {
+        private readonly Dictionary<string, StringValues> _values;
+
+        public KeysBuilder()
+        {
+            _values = new Dictionary<string, StringValues>(StringComparer.OrdinalIgnoreCase);
+        }
+
+        public KeysBuilder With(string key, object value)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            _values[key] = value switch
+            {
+                StringValues sv => sv,
+                string str => str,
+                _ => value.ToString()
+            };
+
+            return this;
+        }
+
+        public IReadOnlyDictionary<string, StringValues> Build() =>
+            new ReadOnlyDictionary<string, StringValues>(_values);
+
+        public static IReadOnlyDictionary<string, StringValues> CreateEmpty() =>
+            new KeysBuilder().Build();
+    }
+}
